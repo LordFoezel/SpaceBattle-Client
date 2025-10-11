aeec11935b42a426838f77cd6da053be4c7ece37
Simon Bertolf <100114824+LordFoezel@users.noreply.github.com>
2025-10-12 00:06:51 +0200

    add

--- src/components/base/alert/BaseAlert.jsx
+++ src/components/base/alert/BaseAlert.jsx
@@ -5,43 +5,35 @@ import { BaseText } from "../text/BaseText";
 import colors from "../../../theme/colors.js";
 
 const BaseAlert = forwardRef(function BaseAlert(
-  {
-    state = "info", // 'info' | 'error' | 'warning' | 'success'
-    text = "",
-    className = "",
-  },
-  ref
+    {
+        state = "info", // 'info' | 'error' | 'warning' | 'success'
+        text = "",
+        className = "",
+    },
+    ref
 ) {
-  const palette = colors?.alert?.[state] ?? colors.alert.info;
-  const [isOpen, setIsOpen] = useState(true);
-  if (!isOpen) return null;
-  return (
-    <Alert
-      status={state}
-      ref={ref}
-      variant="subtle"
-      className={cn(`allert-${state}`, "rounded-md", className, "flex", "flex-row","justify-between","items-center")}
-      size="sm"
-      sx={{
-        bg: palette.bg,
-        color: palette.text,
-        borderWidth: "1px",
-        borderColor: palette.border,
-      }}
-    >
-      <AlertIcon
-        color={palette.icon}
-        sx={{ flexShrink: 0 }}
-      />
-      <BaseText fontSize="xs">{text}</BaseText>
-      
-      <CloseButton
-        pos="relative"
-        insetEnd="-2"
-        onClick={() => setIsOpen(false)}
-      />
-    </Alert>
-  );
+    const palette = colors?.alert?.[state] ?? colors.alert.info;
+    const [isOpen, setIsOpen] = useState(true);
+    if (!isOpen) return null;
+    return (
+        <Alert
+            status={state}
+            ref={ref}
+            variant="subtle"
+            className={cn(`allert-${state}`, "rounded-md", className, "flex", "flex-row", "justify-between", "items-center")}
+            size="sm"
+            sx={{
+                bg: palette.bg,
+                color: palette.text,
+                borderWidth: "1px",
+                borderColor: palette.border,
+            }}
+        >
+            <AlertIcon />
+            <BaseText fontSize="xs">{text}</BaseText>
+            <CloseButton onClick={() => setIsOpen(false)} />
+        </Alert>
+    );
 });
 
 export { BaseAlert };
