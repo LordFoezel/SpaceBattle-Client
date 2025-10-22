5c81cde9f8ee28f4b07e8f1ccec4a21f3bb9f183
Simon Bertolf <100114824+LordFoezel@users.noreply.github.com>
2025-10-22 23:15:26 +0200

    add

--- src/components/layout/MatchListItem.tsx
+++ src/components/layout/MatchListItem.tsx
@@ -20,17 +20,17 @@ const MatchListItem = function MatchListItem(props: MatchListItemProps) {
     return (
         <BaseCard direction='col' variant="light" gap='2' padding="2">
             <TransparentCard direction="row" gap="2">
-                <TransparentCard direction="col" justify="center">
+                <TransparentCard width="full" direction="col" justify="start">
                     <BaseText fontSize="lg">{match.name}</BaseText>
                     <BaseText fontSize="xs" color="gray">{match.description}</BaseText>
                 </TransparentCard>
-                <TransparentCard direction="col" justify="center" gap="0" padding="0" margin="0">
+                <TransparentCard width="1/5" direction="col" justify="center" gap="0" padding="0" margin="0">
                     <TransparentCard direction="row" justify="end" gap="2">
                         {match.password_hash ? <IconLock /> : <IconPeople />}
                         {match.config?.player_count === match.current_player_count ? <BaseText>{globalThis.t("core.full")}</BaseText> : <BaseText>{match.current_player_count}/{match.config?.player_count || "?"}</BaseText>}
                     </TransparentCard>
                 </TransparentCard>
-                <TransparentCard direction="col" justify="center" ><JoinButton matchId={match.id} /></TransparentCard>
+                <TransparentCard width="1/5" direction="col" justify="center" ><JoinButton matchId={match.id} /></TransparentCard>
             </TransparentCard>
         </BaseCard>
     );
