5df02c16fb8f7cc5e8423bffa49274a75a7b8993
LordFoezel <simon.bertolf@bluewin.ch>
2025-11-12 00:27:35 +0100

    add

--- src/components/modal/FleetModal.tsx
+++ src/components/modal/FleetModal.tsx
@@ -2,8 +2,15 @@ import { BaseModal } from "../base/modal/BaseModal";
 import { TransparentCard } from "../layout/TransparentCard";
 import { useEffect, useState } from "react";
 import { Match } from "../../models/match";
-import { BaseDragDrop } from "../base/dragDrop/BaseDragDrop";
-
+import { BaseDragDrop, DragEntity } from "../base/dragDrop/BaseDragDrop";
+import {
+    fetchAll,
+} from "../../repositories/config_fleet_ship";
+import { ConfigFleetShip } from "../../models/config_fleet_ship";
+import { ErrorHelper } from "../../helper/errorHelper";
+import {
+    fetchById,
+} from "../../repositories/ships";
 interface ModalProps {
     onChange?: (e: any) => any;
     match: Match;
@@ -15,21 +22,39 @@ const FleetModal = function FleetModal(props: ModalProps) {
         match,
     } = props;
 
-    const [ships, setShips] = useState([]);
-
-
-    useEffect(() => {
-        if (!match) return;
-        console.log(match.config?.fleet_config_id);
-        // todo: get all ships from config_fleet_ship->shipId repo who is contained in configFleet extend exiting repo if nessesary 
-    }, []);
-
+    const [dragEntity, setDragEntity] = useState<DragEntity[]>([]);
 
     if (!match) return;
+
+    useEffect(() => {
+        if (!match.config) return;
+        loadShipConfigs();
+    }, []);
+
+    async function loadShipConfigs() {
+        try {
+            const data = await fetchAll({ config_fleet_id: match.config.fleet_config_id });
+            const temp: DragEntity[] = [];
+            data.forEach(async (shipConfig, index) => {
+                if (shipConfig.count > 0) {
+                    const ship = await fetchById(shipConfig.ship_id);
+                    let cnt = 0;
+                    while (cnt < shipConfig.count) {
+                        temp.push({ id: `${ship.name}-${index}-${cnt}`, name: ship.name, length: ship.dimension, direction: "horizontal" });
+                        cnt++;
+                    }
+                }
+            });
+            setDragEntity(temp);
+        } catch (error) {
+            ErrorHelper.handleError(error);
+        }
+    }
+
     return (
         <BaseModal buttonText={globalThis.t("fleet.fleet")} title={globalThis.t("fleet.fleetManager")} placement="top" showClose={false} showSave={false}>
             <TransparentCard direction="col" gap="2">
-                <BaseDragDrop sizeX={match.config?.dimension_x ?? 10} sizeY={match.config?.dimension_y ?? 10} onChange={onChange} entities={ships} />
+                <BaseDragDrop sizeX={match.config?.dimension_x} sizeY={match.config?.dimension_y} onChange={onChange} entities={dragEntity} />
             </TransparentCard>
         </BaseModal>
     );
